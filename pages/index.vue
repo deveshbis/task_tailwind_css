3613a56318ef019910b7bbb7891ba916462ac030
deveshbis <deveshbiswas.cse@gmail.com>
2025-01-06 15:29:02 +0600

    create nuxt project

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h1 class="text-5xl text-blue-500 text-center font-bold">Hello World</h1>
+    </div>
+  </template>
+  
+  <!-- <script setup>
+  
+  </script>
+  
+  <style scoped>
+  
+  </style> -->
