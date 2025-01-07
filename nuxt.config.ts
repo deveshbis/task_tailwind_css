513e783d14242a65a8c11e2b0a8d051857d72f5e
deveshbis <deveshbiswas.cse@gmail.com>
2025-01-07 13:06:11 +0600

    make responsive

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules:['nuxt-icon'],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
   css: ['~/assets/css/main.css'],
--- pages/index.vue
+++ pages/index.vue
@@ -5,26 +5,28 @@
       <nav>
         <img
           src="/assets/images/Group.png"
-          class="mx-auto w-44 h-24 mt-4"
+          class="mx-auto lg:w-44 lg:h-24 mt-4 md:w-36 md:h-20 w-32 h-16"
           alt="" />
       </nav>
       <!-- Header section -->
       <header>
+        <!-- <header class="max-w-7xl mx-auto"> -->
         <h1
-          class="text-center text-[60px] text-[#1B4B36] font-['Hind_Siliguri'] font-bold mt-10">
+          class="text-center lg:text-[60px] md:text-[50px] text-[40px] text-[#1B4B36] font-bold mt-10">
           স্বাস্থ্য, ফিটনেস এবং প্রযুক্তি সবকিছু একসাথে! <br />
           "আপনার স্টাইল, আপনার স্মার্টওয়াচ।"
         </h1>
         <div class="flex justify-center items-center">
-          <span class="bg-[#EBFF3D] p-5 text-4xl mt-10 rounded-[5px]">
+          <h2
+            class="bg-[#EBFF3D] lg:p-5 md:p-3 p-2 lg:text-4xl md:text-2xl text-xl mt-10 rounded-[5px]">
             আপনার জীবনকে আরও স্মার্ট করুন।
-          </span>
+          </h2>
         </div>
         <div>
           <iframe
             width="560"
             height="315"
-            class="mx-auto mt-10 w-[1000px] h-[606px]"
+            class="mx-auto mt-10 lg:w-[1000px] lg:h-[606px] md:w-[700px] md:h-[400px] w-[400px] h-[250px]"
             src="https://www.youtube.com/embed/sLAC3SDcmus?si=rvxeNlWoqrNpxNqc"
             title="YouTube video player"
             frameborder="0"
@@ -33,7 +35,7 @@
             allowfullscreen></iframe>
         </div>
         <button
-          class="flex justify-center items-center gap-3 mx-auto mt-5 bg-[#1B4B36] text-[#EBFF3D] px-4 py-2 rounded-3xl text-xl font-bold mb-5">
+          class="flex justify-center items-center gap-3 mx-auto mt-5 bg-[#1B4B36] text-[#EBFF3D] px-4 py-2 rounded-3xl text-xl font-bold mb-5 transition duration-150 ease-in-out hover:bg-[#2C6D4B] hover:scale-105">
           অর্ডার করতে ক্লিক করুন
           <img src="/assets/images/Vector2.png" class="w-7" alt="" />
         </button>
@@ -42,21 +44,25 @@
 
     <!-- Fetures Sections -->
     <div class="bg-[#FAFFCE] mt-10 py-20">
-      <div class="max-w-7xl mx-auto ">
-        <div>
-          <h1 class="text-[68.64px] text-[#1B4B36] font-bold">
+      <div class="max-w-7xl mx-auto">
+        <div class="flex justify-center">
+          <h1
+            class="lg:text-[68.64px] md:text-[45px] text-[30px] sm:text-center text-[#1B4B36] font-bold">
             স্মার্টওয়াচের একাধিক ফিচার <br />
             এক নজরে
           </h1>
         </div>
-        <div class="grid grid-cols-3 gap-4 p-4">
+        <div
+          class="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 lg:gap-4 md:gap-3 gap-2 lg:p-4 md:p-3 p-2">
           <div
             class="border-solid border-2 border-[#1B4B36] w-[406px] h-[350px] rounded-tl-[35px] rounded-br-[35px] px-3 hover:bg-[#1B4B36] hover:text-white group transition duration-1000 ease-in-out">
+            <!-- lg:w-[406px] lg:h-[350px] md:[400px] md:h-[340px] w-[350px] h-[300px] -->
             <h2
-              class="text-4xl text-[#1B4B36] font-semibold mt-5 group-hover:text-[#EBFF3D]">
+              class="lg:text-4xl md:text-2xl text-xl text-[#1B4B36] font-semibold mt-5 group-hover:text-[#EBFF3D]">
               স্বাস্থ্য পর্যবেক্ষণ সুবিধা
             </h2>
-            <p class="text-2xl mt-4 group-hover:text-white">
+            <p
+              class="lg:text-2xl md:text-xl text-lg mt-4 group-hover:text-white">
               হার্ট রেট মনিটর: দিনজুড়ে আপনার হার্টের অবস্থার নজর রাখুন। রক্তের
               অক্সিজেন পর্যবেক্ষণ: SpO2 ট্র্যাকিং দিয়ে স্বাস্থ্য সম্পর্কে সচেতন
               থাকুন। স্লিপ ট্র্যাকিং: গভীর ও হালকা ঘুমের স্তর বিশ্লেষণ করে সঠিক
@@ -66,10 +72,11 @@
           <div
             class="border-solid border-2 border-[#1B4B36] w-[406px] h-[350px] rounded-tl-[35px] rounded-br-[35px] px-3 hover:bg-[#1B4B36] hover:text-white group transition duration-1000 ease-in-out">
             <h2
-              class="text-4xl text-[#1B4B36] font-semibold mt-5 group-hover:text-[#EBFF3D]">
+              class="lg:text-4xl md:text-2xl text-xl text-[#1B4B36] font-semibold mt-5 group-hover:text-[#EBFF3D]">
               স্বাস্থ্য পর্যবেক্ষণ সুবিধা
             </h2>
-            <p class="text-2xl mt-4 group-hover:text-white">
+            <p
+              class="lg:text-2xl md:text-xl text-lg mt-4 group-hover:text-white">
               হার্ট রেট মনিটর: দিনজুড়ে আপনার হার্টের অবস্থার নজর রাখুন। রক্তের
               অক্সিজেন পর্যবেক্ষণ: SpO2 ট্র্যাকিং দিয়ে স্বাস্থ্য সম্পর্কে সচেতন
               থাকুন। স্লিপ ট্র্যাকিং: গভীর ও হালকা ঘুমের স্তর বিশ্লেষণ করে সঠিক
@@ -79,10 +86,11 @@
           <div
             class="border-solid border-2 border-[#1B4B36] w-[406px] h-[350px] rounded-tl-[35px] rounded-br-[35px] px-3 hover:bg-[#1B4B36] hover:text-white group transition duration-1000 ease-in-out">
             <h2
-              class="text-4xl text-[#1B4B36] font-semibold mt-5 group-hover:text-[#EBFF3D]">
+              class="lg:text-4xl md:text-2xl text-xl text-[#1B4B36] font-semibold mt-5 group-hover:text-[#EBFF3D]">
               ফিটনেস ট্র্যাকিং
             </h2>
-            <p class="text-2xl mt-4 group-hover:text-white">
+            <p
+              class="lg:text-2xl md:text-xl text-lg mt-4 group-hover:text-white">
               স্পোর্টস মোড: বিভিন্ন ধরনের স্পোর্টস অ্যাক্টিভিটির জন্য ১০+ মোড।
               ক্যালোরি বার্ন কাউন্টার: আপনার ব্যায়ামের সময় ক্যালোরি খরচ
               নির্ধারণ করুন। স্টেপ ট্র্যাকার: প্রতিদিনের পদক্ষেপ হিসেব করুন।
@@ -91,10 +99,11 @@
           <div
             class="border-solid border-2 border-[#1B4B36] w-[406px] h-[350px] rounded-tl-[35px] rounded-br-[35px] px-3 hover:bg-[#1B4B36] hover:text-white group transition duration-1000 ease-in-out">
             <h2
-              class="text-4xl text-[#1B4B36] font-semibold mt-5 group-hover:text-[#EBFF3D]">
+              class="lg:text-4xl md:text-2xl text-xl text-[#1B4B36] font-semibold mt-5 group-hover:text-[#EBFF3D]">
               স্মার্ট নোটিফিকেশন
             </h2>
-            <p class="text-2xl mt-4 group-hover:text-white">
+            <p
+              class="lg:text-2xl md:text-xl text-lg mt-4 group-hover:text-white">
               কল, মেসেজ, এবং অ্যাপ নোটিফিকেশন সরাসরি আপনার হাতে। গুরুত্বপূর্ণ
               আপডেট কখনোই মিস করবেন না।
             </p>
@@ -102,20 +111,22 @@
           <div
             class="border-solid border-2 border-[#1B4B36] w-[406px] h-[350px] rounded-tl-[35px] rounded-br-[35px] px-3 hover:bg-[#1B4B36] hover:text-white group transition duration-1000 ease-in-out">
             <h2
-              class="text-4xl text-[#1B4B36] font-semibold mt-5 group-hover:text-[#EBFF3D]">
+              class="lg:text-4xl md:text-2xl text-xl text-[#1B4B36] font-semibold mt-5 group-hover:text-[#EBFF3D]">
               দীর্ঘস্থায়ী ব্যাটারি লাইফ
             </h2>
-            <p class="text-2xl mt-4 group-hover:text-white">
+            <p
+              class="lg:text-2xl md:text-xl text-lg mt-4 group-hover:text-white">
               একবার চার্জে ৭+ দিন ব্যবহারের সুবিধা। দ্রুত চার্জিং প্রযুক্তি।
             </p>
           </div>
           <div
             class="border-solid border-2 border-[#1B4B36] w-[406px] h-[350px] rounded-tl-[35px] rounded-br-[35px] px-3 hover:bg-[#1B4B36] hover:text-white group transition duration-1000 ease-in-out">
             <h2
-              class="text-4xl text-[#1B4B36] font-semibold mt-5 group-hover:text-[#EBFF3D]">
+              class="lg:text-4xl md:text-2xl text-xl text-[#1B4B36] font-semibold mt-5 group-hover:text-[#EBFF3D]">
               মিউজিক এবং ক্যামেরা নিয়ন্ত্রণ
             </h2>
-            <p class="text-2xl mt-4 group-hover:text-white">
+            <p
+              class="lg:text-2xl md:text-xl text-lg mt-4 group-hover:text-white">
               মিউজিক চালানো বা ক্যামেরা ক্লিক করুন আপনার স্মার্টওয়াচ থেকে। সহজ
               এবং সাবলীল ব্যবহারের সুবিধা।
             </p>
@@ -123,7 +134,7 @@
         </div>
       </div>
       <button
-        class="flex justify-center items-center gap-3 mx-auto mt-5 bg-[#1B4B36] text-[#EBFF3D] px-4 py-2 rounded-3xl text-xl font-bold transition duration-700 ease-in-out">
+        class="flex justify-center items-center gap-3 mx-auto mt-5 bg-[#1B4B36] text-[#EBFF3D] px-4 py-2 rounded-3xl text-xl font-bold mb-5 transition duration-150 ease-in-out hover:bg-[#2C6D4B] hover:scale-105">
         অর্ডার করতে ক্লিক করুন
         <img src="/assets/images/Vector2.png" class="w-7" alt="" />
       </button>
@@ -133,43 +144,109 @@
     <div class="mt-20 mb-20">
       <div class="max-w-7xl mx-auto">
         <div class="mt-10 mb-10">
-          <h1 class="text-[62px] text-[#1B4B36] font-bold">
+          <h1
+            class="lg:text-[62px] md:text-[45px] text-[30px] text-center text-[#1B4B36] font-bold">
             আমাদের পণ্য নিয়ে গ্রাহকদের অভিজ্ঞতা
           </h1>
         </div>
-        <div class="grid grid-cols-2 gap-4 p-4">
-          <div class="border-solid border-2 border-[#1B4B36] w-[567px] h-[325px] px-5 py-4 bg-[#1B4B36] rounded-[20px]">
-            <h2 class="text-[48px] text-white font-bold">সন্তুষ্ট গ্রাহক</h2>
-            <p class="text-[90px] text-[#EBFF3D] text-end">৩,০০০+</p>
+        <div
+          class="grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 lg:gap-4 md:gap-3 gap-2 lg:p-4 md:p-3 p-2">
+          <div
+            class="border-solid border-2 border-[#1B4B36] lg:w-[567px] lg:h-[325px] md:w-[500px] md:h-[450px] w-[450px] h-[350px] px-5 py-4 bg-[#1B4B36] rounded-[20px]">
+            <h2
+              class="lg:text-[48px] md:text-4xl text-2xl text-white font-bold">
+              সন্তুষ্ট গ্রাহক
+            </h2>
+            <p
+              class="lg:text-[90px] md:text-[70px] text-[50px] text-[#EBFF3D] text-end">
+              ৩,০০০+
+            </p>
             <hr />
-            <h3 class="text-xl mt-5 text-white font-semibold">সন্তুষ্ট গ্রাহকরা আমাদের পণ্যের প্রতি বিশ্বাস রেখেছেন।</h3>
-
+            <h3 class="text-xl lg:mt-5 md:mt-3 mt-2 text-white font-semibold">
+              সন্তুষ্ট গ্রাহকরা আমাদের পণ্যের প্রতি বিশ্বাস রেখেছেন।
+            </h3>
           </div>
-          <div class="border-solid border-2 border-[#1B4B36] w-[567px] h-[325px] px-5 py-4 bg-[#1B4B36] rounded-[20px]">
-            <h2 class="text-[48px] text-white font-bold">পুনঃক্রয়কারী</h2>
-            <p class="text-[90px] text-[#EBFF3D] text-end">৪০০+</p>
+          <div
+            class="border-solid border-2 border-[#1B4B36] lg:w-[567px] lg:h-[325px] md:w-[500px] md:h-[450px] w-[450px] h-[350px] px-5 py-4 bg-[#1B4B36] rounded-[20px]">
+            <h2
+              class="lg:text-[48px] md:text-4xl text-2xl text-white font-bold">
+              পুনঃক্রয়কারী
+            </h2>
+            <p
+              class="lg:text-[90px] md:text-[70px] text-[50px] text-[#EBFF3D] text-end">
+              ৪০০+
+            </p>
             <hr />
-            <h3 class="text-xl mt-5 text-white font-semibold">পুনঃক্রয় করে আমাদের গুণমান নিশ্চিত করে।</h3>
+            <h3 class="text-xl lg:mt-5 md:mt-3 mt-2 text-white font-semibold">
+              পুনঃক্রয় করে আমাদের গুণমান নিশ্চিত করে।
+            </h3>
           </div>
-          <div class="border-solid border-2 border-[#1B4B36] w-[567px] h-[325px] px-5 py-4 bg-[#1B4B36] rounded-[20px]">
-            <h2 class="text-[48px] text-white font-bold">পণ্য বিক্রি</h2>
-            <p class="text-[90px] text-[#EBFF3D] text-end">৫০০০+</p>
+          <div
+            class="border-solid border-2 border-[#1B4B36] lg:w-[567px] lg:h-[325px] md:w-[500px] md:h-[450px] w-[450px] h-[350px] px-5 py-4 bg-[#1B4B36] rounded-[20px]">
+            <h2
+              class="lg:text-[48px] md:text-4xl text-2xl text-white font-bold">
+              পণ্য বিক্রি
+            </h2>
+            <p
+              class="lg:text-[90px] md:text-[70px] text-[50px] text-[#EBFF3D] text-end">
+              ৫০০০+
+            </p>
             <hr />
-            <h3 class="text-xl mt-5 text-white font-semibold">৫০০০ পণ্যের বিক্রি আমাদের জনপ্রিয়তা প্রমাণ করে।</h3>
+            <h3 class="text-xl lg:mt-5 md:mt-3 mt-2 text-white font-semibold">
+              ৫০০০ পণ্যের বিক্রি আমাদের জনপ্রিয়তা প্রমাণ করে।
+            </h3>
           </div>
-          <div class="border-solid border-2 border-[#1B4B36] w-[567px] h-[325px] px-5 py-4 bg-[#1B4B36] rounded-[20px]">
-            <h2 class="text-[48px] text-white font-bold">গ্রাহক সন্তুষ্ট</h2>
-            <p class="text-[90px] text-[#EBFF3D] text-end">৯৫%</p>
+          <div
+            class="border-solid border-2 border-[#1B4B36] lg:w-[567px] lg:h-[325px] md:w-[500px] md:h-[450px] w-[450px] h-[350px] px-5 py-4 bg-[#1B4B36] rounded-[20px]">
+            <h2
+              class="lg:text-[48px] md:text-4xl text-2xl text-white font-bold">
+              গ্রাহক সন্তুষ্ট
+            </h2>
+            <p
+              class="lg:text-[90px] md:text-[70px] text-[50px] text-[#EBFF3D] text-end">
+              ৯৫%
+            </p>
             <hr />
-            <h3 class="text-xl mt-5 text-white font-semibold">৯৫% গ্রাহক আমাদের সেবা ও পণ্যে সন্তুষ্ট।</h3>
+            <h3 class="text-xl lg:mt-5 md:mt-3 mt-2 text-white font-semibold">
+              ৯৫% গ্রাহক আমাদের সেবা ও পণ্যে সন্তুষ্ট।
+            </h3>
           </div>
         </div>
       </div>
       <button
-        class="flex justify-center items-center gap-3 mx-auto mt-5 bg-[#EBFF3D] text-[#1B4B36] px-4 py-2 rounded-3xl text-xl font-bold mb-5 transition duration-700 ease-in-out">
+        class="flex justify-center items-center gap-3 mx-auto mt-5 bg-[#EBFF3D] text-[#1B4B36] px-4 py-2 rounded-3xl text-xl font-bold mb-5 transition duration-150 ease-in-out  hover:scale-105">
         অর্ডার করতে ক্লিক করুন
         <img src="/assets/images/Vector.png" class="w-7" alt="" />
       </button>
+    </div>
+
+    <!-- Footer Section -->
+    <div>
+      <div
+        class="bg-[#0C3200] flex flex-col sm:flex-row justify-around items-center h-32 px-4 sm:px-8">
+        <h2 class="text-white flex items-center gap-2 text-center sm:text-left">
+          <Icon
+            name="fa6-regular:address-card"
+            class="w-6 h-9 text-white rounded-lg" />
+          +880 123 456 790
+        </h2>
+        <img src="/assets/images/logo2.png" class="w-24 mt-4 sm:mt-0" alt="" />
+        <div class="flex gap-3 mt-4 sm:mt-0">
+          <Icon name="uiw:facebook" class="w-5 h-9 text-white rounded-lg" />
+          <Icon
+            name="entypo-social:twitter-with-circle"
+            class="w-6 h-9 text-white rounded-lg" />
+          <Icon
+            name="entypo-social:linkedin-with-circle"
+            class="w-6 h-9 text-white rounded-lg" />
+        </div>
+      </div>
+      <div class="bg-[#0A2900] h-16">
+        <h2 class="text-center text-white pt-5 text-sm">
+          "© 2024 All Rights Reserved by BDFunnelBuilder. Unauthorized use
+          prohibited."
+        </h2>
+      </div>
     </div>
   </div>
 </template>
